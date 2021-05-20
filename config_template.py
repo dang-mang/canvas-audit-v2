37469e8109a2e2a9cf442e41ff34a1604cb1d593
Collin <collinmuurphy@gmail.com>
2021-05-20 11:52:01 -0600

    updated config template

--- config_template.py
+++ config_template.py
@@ -1,6 +1,4 @@
 #fill out this file with the proper information, then rename it as "config.py" in this directory
 token = "" #API key goes here
-URL = [] "" #URL goes here
-filename = "input.json" #Filename and directory is set in these fields. Do not change unless you know what you are doing
-directory = "."
+URL = [] #URL goes here. To make work, you have to split up the URL into 2 parts, and I can show you how to do that for the specific use case this was made for
 
