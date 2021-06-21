4ae57720d200fe0314a1b27c7457577ff23fac4f
Collin <collinmuurphy@gmail.com>
2021-06-21 14:11:46 -0600

    csv files created for both logins and general info

--- fix_json.py
+++ fix_json.py
@@ -2,21 +2,20 @@ import os
 import sys
 import fileinput
 
-def fix_json(input_filename='input.json', directory = "data/", output_filename = "output.csv"):
+def fix_json(input_filename='input.json', directory = "data/", storage_path = "data/first/", output_filename = "output.csv"):
     output_path = directory + output_filename
-    input_path = directory + input_filename
-    clean_path = directory + 'input_clean.json'
+    input_path = storage_path + input_filename
+    clean_path = storage_path + input_filename.split(".")[0] + "_clean.json"
     remove_empty_lines(input_path, clean_path)
-    with open(clean_path) as f:
-        #newText=f.read().replace('}{', '},\n{').replace("]\n[",",").replace(",\n]","\n]")
-        newText=f.read().replace('[', '').replace("]","").replace('}\n{','},\n{').replace('}{', '},\n{').replace(',\n,',',\n')
-        newText= newText.replace('[', '').replace("]","")
-        newText= "[{0}]".format(newText)
+    with open(input_path) as f:
+        #new_text=f.read().replace('}{', '},\n{').replace("]\n[",",").replace(",\n]","\n]")
+        #new_text=f.read().replace('[', '').replace(']','').replace('}\n{','},\n{').replace('}{', '},\n{').replace(',\n,',',\n')
+        new_text=f.read().replace('][', '],\n[').replace('}{', '},\n{')
         
-    with open(input_path, "w") as f:
-        f.write(newText)
-    
-    os.system(f"python3 json_to_csv.py l {input_path} {output_path}")
+    with open(clean_path, "w") as f:
+        f.write(new_text)
+
+    os.system(f"python3 json_to_csv.py l {clean_path} {output_path}")
 
 def remove_empty_lines(input_path, clean_path):
     char = '{'
--- get_admins.py
+++ get_admins.py
@@ -2,65 +2,75 @@ import os
 import subprocess
 import pandas as pd
 from config import *
+from pathlib import Path
 from fix_json import *
+from fix_csv import *
 from merge_csv import *
 from concat_IDs import *
 directory = "data/"
-input_filename = directory + "input.json"
-output_filename = directory + "output.csv"
+input_filename = "input.json"
+output_filename = "output.csv"
 
 def get_command(URL_to_use, sys_id,out = input_filename):
-    #generate command with sys id
     return f"curl -H \"Authorization: Bearer {token}\" \"{URL_to_use[0]}{sys_id}{URL_to_use[1]}\" >> {out}"
 
 def get_IDs(): 
     #get data from source
-    data = pd.read_csv(directory + "source.csv", header=0)
+    data = pd.read_csv("source.csv", header=0)
     sys_IDs = list(data['canvas_user_id'].to_list())
     num_users = len(sys_IDs) 
     print(f"Found {num_users} users in source.csv.\nPreparing to download data from {URL[0]}")
     return sys_IDs
 
-def write_to_file(URL_to_use,sys_IDs, out = input_filename, csv = output_filename):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    os.system(f"echo \"[\" >> {out}")
+def write_to_file(URL_to_use,sys_IDs, inp = input_filename, csv = output_filename, destination = directory):
+    inp = destination + inp
+    os.system(f"echo \"[\" >> {inp}")
     num_users = len(sys_IDs) 
     for n in sys_IDs:
-        os.system(get_command(URL_to_use, n, out))
+        os.system(get_command(URL_to_use, n, inp))
         if n != sys_IDs[-1]:
-            os.system(f"echo \",\" >> {out}")    
+            os.system(f"echo \",\" >> {inp}")    
         percent = sys_IDs.index(n)/num_users * 100 
         print(f"Download progress - {round(percent,2)}% ({sys_IDs.index(n)}/{num_users})")
-    os.system(f"echo \"]\" >> {out}")
-    input_string = input_filename.split("/")[1]
+    os.system(f"echo \"]\" >> {inp}")
+    input_string = inp.split("/")[1]
 
     print(f"Finished downloading {num_users} rows of user data.\nNow converting {input_string} to {csv}")
 
-def clean_files():
-    keep = "source.csv"
-    
-    for filename in os.listdir(directory):
-        if filename != keep:
-            os.remove(os.path.join(directory + filename))
-
+def clean_files(path):  
+    for f in os.listdir(path):
+        os.remove(os.path.join(path, f))
+ 
 def main():
+    #get sys ids from source file
     sys_IDs = get_IDs()
-    write_to_file(URL, sys_IDs)
+    
+    #prep
+    first = directory + "first/"
+    os.makedirs(first, exist_ok=True)
+    clean_files(first)
+
+    #first API call for IDs
+    write_to_file(URL, sys_IDs, destination = first)
     print("Fixing JSON and converting to CSV...")
     fix_json()
-
-    #print("CSV file created.")
+    print("CSV file created.")
+    
+    #merge source and generated
     #print("Merging source file and output file...")
     #merge_csv()
-
-    #print("CSV file merged.\nFetching IDs from API...")
+    #print("CSV file merged.")
+    
+    #get i-numbers from API
+    print("Fetching IDs from API...")
     login_in = "logins.json"
     login_out = "logins.csv"
-    write_to_file(URL_LOGIN, sys_IDs,out = "data/" + login_in, csv = login_out)
-    #fix_json(input_filename = login_in,output_filename = login_out)
-    print("IDs fetched from API.\nMerging final CSV file...")
+    write_to_file(URL_LOGIN, sys_IDs, inp = login_in, destination = first, csv = login_out)
+    fix_json(input_filename = login_in ,output_filename = login_out)
+    print("IDs fetched from API.")
+
+    #print("Merging final CSV file...")
+    #fix_csv()
 
     #merge_csv(source_filename = 'id.csv', output_filename = 'merged.csv', final = 'admins_final.csv', first = 'l_email' ,last = 'l_email')
     #print("Final CSV file successfully generated. Cleaning files...")
